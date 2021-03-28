5c4221a9beef844c65eded5923ec37d5114a4a01
weeer <kzs1845@gmail.com>
2021-03-28 14:24:11 +0300

    try

--- js/form.js
+++ js/form.js
@@ -1,10 +1,16 @@
 const adFormElement = document.querySelector('.ad-form');
+const inputTitle = adFormElement.querySelector('#title');
+
+const MIN_NAME_LENGTH = 30;
+const MAX_NAME_LENGTH = 100;
 
 const elementsForm = {
   typeElement: adFormElement.querySelector('#type'),
   priceElement: adFormElement.querySelector('#price'),
   checkinElement: adFormElement.querySelector('#timein'),
   checkoutElement: adFormElement.querySelector('#timeout'),
+  roomNumber: adFormElement.querySelector('#room_number'),
+  capacityElement: adFormElement.querySelector('#capacity'),
 }
 
 const PRICES = {
@@ -13,7 +19,6 @@ const PRICES = {
   house: 5000,
   palace: 10000,
 }
-
 
 // тип жилья
 const formTypeChangeHandler = () => {
@@ -41,7 +46,6 @@ function addFormTimeOutEventListener(){
 
 addFormTimeOutEventListener();
 
-
 // заезд после
 const formTimeInChangeHandler = () => {
   elementsForm.checkoutElement.addEventListener('change', () => {
@@ -54,3 +58,81 @@ function addFormTimeInEventListener(){
 }
 
 addFormTimeInEventListener();
+
+//Валидация заголовка объявления
+inputTitle.addEventListener('input', () => {
+  const valueLength = inputTitle.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    inputTitle.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    inputTitle.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) +' симв.');
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+
+  inputTitle.reportValidity();
+});
+
+// валидация цены за ночь
+elementsForm.priceElement.addEventListener('input', () => {
+  const value = elementsForm.priceElement.value;
+  const maxValue = 1000000;
+
+  if (value > maxValue) {
+    elementsForm.priceElement.setCustomValidity('Значение не может быть больше 1,000,000')
+  }
+
+  elementsForm.priceElement.reportValidity();
+});
+
+// количество комнат и количество мест
+const roomNumber = elementsForm.roomNumber;
+const guestNumber = elementsForm.capacityElement;
+const noGuests = {
+  value: 100,
+  text: 'не для гостей',
+};
+
+const oneGuests = {
+  value: 1,
+  text: 'для одного гостя',
+};
+
+const twoGuests = {
+  value: 2,
+  text: 'для 2 гостей',
+};
+
+const threeGuests = {
+  value: 3,
+  text: 'для 3 гостей',
+};
+
+const options = {
+  100: [noGuests],
+  1: [oneGuests],
+  2: [twoGuests, oneGuests],
+  3: [threeGuests, twoGuests, oneGuests],
+};
+
+const getOptions = function (guests) {
+  for (let i = 0; i < guests.length; i++) {
+    const option = document.createElement('option');
+    option.value = guests[i].value;
+    option.innerHTML = guests[i].text;
+    guestNumber.appendChild(option);
+  }
+};
+
+// зависимость количества комнат от количества мест
+roomNumber.addEventListener('change', function () {
+  const roomNumberValue = roomNumber.value;
+  guestNumber.value = (roomNumberValue === '100') ? '0' : roomNumberValue;
+
+  while (guestNumber.firstChild) {
+    guestNumber.removeChild(guestNumber.firstChild);
+  }
+
+  getOptions(options[roomNumberValue]);
+});
